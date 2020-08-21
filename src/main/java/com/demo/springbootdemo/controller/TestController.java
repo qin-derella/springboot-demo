df0a21d4eb1b8e14856b73d1886b91d68c9c160a
刘月芹 <liuyueqin@17of.com>
2020-08-21 16:33:35 +0800

    springboot整合swagger

--- src/main/java/com/demo/springbootdemo/controller/TestController.java
+++ src/main/java/com/demo/springbootdemo/controller/TestController.java
@@ -1,18 +1,22 @@
 package com.demo.springbootdemo.controller;
 
 import com.demo.springbootdemo.config.UrlConfigs;
-import org.springframework.web.bind.annotation.RequestMapping;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@Api(tags = "测试相关接口")
 public class TestController {
 
     @Resource
     UrlConfigs urlConfigs;
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
+    @ApiOperation("测试【lyq】")
     public String test() {
         return urlConfigs.getQueryUserInfo();
     }
